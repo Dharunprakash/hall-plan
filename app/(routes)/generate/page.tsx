18fd48fb17d173dac0f019e283105118d222ecfe
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-05-17 15:33:17 +0530

    Added delete exam

--- app/(routes)/generate/page.tsx
+++ app/(routes)/generate/page.tsx
@@ -1,5 +1,8 @@
+import { DeleteIcon } from "lucide-react"
+
 import { serverClient } from "@/app/_trpc/serverClient"
 
+import DeleteExam from "./_components/delete-exam"
 import ExamCard from "./_components/exam-card"
 import ExamPagination from "./_components/exam-pagination"
 import TopBar from "./_components/top-bar"
@@ -20,7 +23,10 @@ const page = async ({
       <TopBar />
       <div className="mx-auto flex max-h-[65vh] max-w-7xl flex-col overflow-y-auto">
         {exams.map((exam) => (
+          // <div className="flex " key={exam.id}>
           <ExamCard key={exam.id} exam={exam} />
+          //   <DeleteExam id={exam.id} />
+          // </div>
         ))}
       </div>
       <ExamPagination />
