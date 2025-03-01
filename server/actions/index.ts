7cbb8871ede801a0495940639f55993f8ae3e7c0
dharun <dharunprakash.m1504@gmail.com>
2025-03-01 13:27:54 +0530

    schema modified

--- server/actions/index.ts
+++ server/actions/index.ts
@@ -64,7 +64,7 @@ export const createExam = async (input: z.infer<typeof GenerateHallSchema>) => {
         data: {
           academicYear: examDetails.academicYear,
           name: examDetails.name,
-          Arrangementtype: hallType,
+          arrangementtype: hallType,
           semester: examDetails.semester,
           departmentId:
             examDetails.type === "INTERNAL" ||
